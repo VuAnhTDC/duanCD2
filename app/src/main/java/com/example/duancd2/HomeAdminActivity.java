b98e465e61612e626f798d3682e6191ebf974a1c
VuAnhTDC <21211tt1568@mail.tdc.edu.vn>
2023-10-17 23:07:04 +0700

    Màn hình home của admin

--- app/src/main/java/com/example/duancd2/HomeAdminActivity.java
+++ app/src/main/java/com/example/duancd2/HomeAdminActivity.java
@@ -1,14 +1,36 @@
 package com.example.duancd2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class HomeAdminActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_admin);
+        setControl();
+        setEvent();
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_homeadmin,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //    Anh xa
+    private void setControl(){
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+//    Su kien
+    private void setEvent(){
+
     }
 }
